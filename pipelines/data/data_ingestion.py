8f94977f50bfb463a35fd4618a0ed9a4c9289345
MitadruMridha05 <24me01059@iitbbs.ac.in>
2026-09-24 00:19:10 +0530

    successfully completed data ingestion

--- pipelines/data/data_ingestion.py
+++ pipelines/data/data_ingestion.py
@@ -11,13 +11,98 @@ from sklearn.model_selection import train_test_split
 
 # Logging Configuration
 logger = logging.getLogger("data_ingestion")
-logger.setLevel(logging.INFO)
-logger.propagate = False
+logger.setLevel(logging.DEBUG)
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.ERROR)
 
-file_handler = logging.FileHandler("data_ingestion.log")
-file_handler.setLevel(logging.INFO)
+file_handler = logging.FileHandler("errors.log")
+file_handler.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+	"""Load pipeline parameters from a YAML file."""
+	try:
+		with open(params_path, "r", encoding="utf-8") as params_file:
+			params = yaml.safe_load(params_file)
+		logger.debug("Parameters retrieved from %s", params_path)
+		return params
+	except FileNotFoundError:
+		logger.error('File not found: %s', params_path)
+		raise
+	except (OSError, yaml.YAMLError) as exc:
+		logger.error("Unable to load parameters from %s: %s", params_path, exc)
+		raise
+	except Exception as e:
+		logger.error('Unexpected error: %s', e)
+		raise
+
+def load_data(data_url: str) -> pd.DataFrame:
+	"""Load data from a csv file"""
+	try:
+		df = pd.read_csv(data_url)
+		logger.debug('Data loaded from %s', data_url)
+		return df
+	except pd.errors.ParserError as e:
+		logger.error('Failed to parse the csv file: %s', e)
+		raise
+	except Exception as e:
+		logger.error('Unexpected error occured while loading the data: %s', e)
+		raise
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+	"""Preprocess the data by handling missing values, duplicates, and empty strings."""
+	try:
+		df.dropna(inplace=True)
+		df.drop_duplicates(inplace=True)
+		df=df[df["CommentText"].str.strip() != '']
+		logger.debug('Data preprocessing completed: Missing values, duplicates and empty strings removed.')
+		return df
+	
+	except KeyError as e:
+		logger.error('Missing column in the dataframe: %s', e)
+		raise
+	except Exception as e:
+		logger.error("Unexpected erroe during preprocessing: %s", e)
+		raise
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
+	"""Save the train and test datasets, creating the raw folder if it doesn't exist."""
+	try:
+		raw_data_path = os.path.join(data_path, 'raw')
+		os.makedirs(raw_data_path, exist_ok=True)
+		train_data.to_csv(os.path.join(raw_data_path,"train.csv"), index=False)
+		test_data.to_csv(os.path.join(raw_data_path,"test.csv"), index=False)
+		logger.debug('Train and test data saved to %s', raw_data_path)
+	except Exception as e:
+		logger.error('Unexpected error occurred while saving the data: %s', e)
+		raise
+
+def main():
+	try:
+		params = load_params(params_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../params.yaml'))
+		test_size = params['data_ingestion']['test_size']
+
+		df = load_data(data_url=r'C:\Users\mitad\OneDrive\Documents\coding_maxxing\sentiment_analysing_project\artifacts\youtube_comments_cleaned_final.csv')  # replace with your actual source path/URL
+		final_df = preprocess_data(df)
+
+		train_data, test_data = train_test_split(
+			final_df, test_size=test_size, random_state=42
+		)
+
+		save_data(train_data, test_data, data_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'../../data'))
+
+	except Exception as e:
+		logger.error('Failed to complete the data ingestion process: %s', e)
+		print(f"Error: {e}")
 
 
+if __name__ == '__main__':
+	main()
+		
